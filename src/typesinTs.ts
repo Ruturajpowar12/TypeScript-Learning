d3d7ac79d2a178071750310086e172471d5edc84
Ruturajpowar12 <ruturajpowar2038@gmail.com>
2026-09-25 16:47:49 +0530

    inference and annotation in typescript

---
+++ src/typesinTs.ts
@@ -0,0 +1,10 @@
+// let name = "ruturaj"
+
+// let nums = Math.random() > 0.6 ? 10 : "4" // type inferencing(Auto-understandable)
+
+// let hobby:string = "playing cricket" // type annotation (Needs to understand for type !!)
+// hobby = "playing hockey"
+
+let isValid: boolean = true;
+isValid = false;
+console.log(isValid);
